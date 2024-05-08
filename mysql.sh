15825f1d4ffbd70b938abcf7178d20bec6bc7ef7
HP <ammuraj4466@gmail.com>
2024-05-08 20:20:09 +0530

    making shell scripting topics. Little underlining###
    frontend script

--- mysql.sh
+++ mysql.sh
@@ -3,21 +3,21 @@ color="\e[36m"
 
 echo -e "${color} Disable MySQL default Version \e[0m"
 dnf module disable mysql -y &>>$log_file
-echo &?
+echo $?
 
 echo -e "${color} Copy MySQL Repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo &?
+echo $?
 
 echo -e "${color} Install MySQL Server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo &?
+echo $?
 
 echo -e "${color} Start MySQL Server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo &?
+echo $?
 
 echo -e "${color}SEt MySQl Password \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo &?
+echo $?
